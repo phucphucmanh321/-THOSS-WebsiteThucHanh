97aee768ec4e67aefe2a70884bdc11375cb79364
PhamTrungHiep <phiep725@gmail.com>
2025-12-16 17:42:13 +0700

    fix: fix loi hinh anh danh sach san pham

--- config/config.php
+++ config/config.php
@@ -1,13 +1,14 @@
 <?php
 $configDB = array();
-$configDB["host"] 		= "qlbh.cl826620slbt.ap-southeast-2.rds.amazonaws.com";
+$configDB["host"] 		= "localhost";
 $configDB["database"]	= "shop_quanao";
 $configDB["username"] 	= "root";
-$configDB["password"] 	= "Phuc1522004";
-define("HOST", "qlbh.cl826620slbt.ap-southeast-2.rds.amazonaws.com");
+$configDB["password"] 	= "";
+define("HOST", "localhost");
 define("DB_NAME", "shop_quanao");
 define("DB_USER", "root");
-define("DB_PASS", "Phuc1522004");
+define("DB_PASS", "");
 define('ROOT', dirname(dirname(__FILE__) ) );
 //Thu muc tuyet doi truoc cua config; c:/wamp/www/lab/
-define("BASE_URL", "http://".$_SERVER['SERVER_NAME']."/");//dia chi website
+define("BASE_URL", "http://".$_SERVER['SERVER_NAME']."/-THOSS-WebsiteThucHanh");
+//dia chi website
--- module/product/index.php
+++ module/product/index.php
@@ -1,7 +1,7 @@
 <?php
 	$product = new Product();
 	$ac=getIndex("ac", "list");
-
+	
 	if ($ac=="list")
 		{
 			include ROOT."/module/product/list.php";
