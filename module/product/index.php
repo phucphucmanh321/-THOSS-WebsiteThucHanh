589eaa47e38c02c297ac89a3e2c9aa39b8021ec9
PhamTrungHiep <phiep725@gmail.com>
2025-12-16 16:58:36 +0700

    feat: them chuc nang xem danh sach san pham

--- module/product/index.php
+++ module/product/index.php
@@ -1,17 +1,10 @@
 <?php
 	$product = new Product();
 	$ac=getIndex("ac", "list");
-	if ($ac=="home")
-		{
-			include ROOT."/module/home/index.php";
-		}
+
 	if ($ac=="list")
 		{
 			include ROOT."/module/product/list.php";
 		}
-	if ($ac=="detail")
-		{
-			$id=getIndex("id", "");
-			include ROOT."/module/product/detail.php";	
-		}
+
 ?>
