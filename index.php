fd4233e91323812f713eb6fc5b1e5257e22b45c2
Khang Phan <vykhang2207@gmail.com>
2025-12-18 19:29:08 +0700

    update: cap nhat routing cho module account

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 
-	include "./config/config.php";
+include "./config/config.php";
 
-	// Nạp file chứa các hàm tiện ích dùng chung
-	include ROOT."/include/function.php";
+// Nạp file chứa các hàm tiện ích dùng chung
+include ROOT . "/include/function.php";
 
-	// Đăng ký hàm autoload: khi khởi tạo một class, PHP sẽ tự động tìm và load file class tương ứng
-	spl_autoload_register("loadClass");
+// Đăng ký hàm autoload: khi khởi tạo một class, PHP sẽ tự động tìm và load file class tương ứng
+spl_autoload_register("loadClass");
 
-	// Khởi tạo đối tượng kết nối CSDL (class Db sẽ được tự động load từ file Db.class.php)
-	$db = new Db();
-    session_start();
+// Khởi tạo đối tượng kết nối CSDL (class Db sẽ được tự động load từ file Db.class.php)
+$db = new Db();
+session_start();
 
-    
- 	include_once "include/header.php";
+
+include_once "include/header.php";
 ?>
 
 <h1 class="hidden">Chuột Trắng - </h1>
@@ -23,64 +23,63 @@
 
 <div class="contain">
     <?php
-		$mod = getIndex("mod","home");
-        
-        switch($mod)
-        {
-            case "product":
-                include "module/product/index.php";
-                break;
-            case "cart":
-                include "module/cart/index.php";
-                break;
-            default:
-                include "module/home/index.php";
-                break;
-        }
-	?>
+    $mod = getIndex("mod", "home");
+
+    switch ($mod) {
+        case "product":
+            include "module/product/index.php";
+            break;
+        case "cart":
+            include "module/cart/index.php";
+            break;
+        default:
+            include "module/home/index.php";
+            break;
+    }
+    ?>
 </div>
 
 <script>
-$('.home-slider').slick({
-    lazyLoad: 'ondemand',
-    centerMode: false,
-    focusOnSelect: true,
-    dots: false,
-    loop: true,
-    infinite: true,
-    autoplaySpeed: 3000,
-    autoplay: true,
-    dots: false,
-    arrows: true,
-    slidesToShow: 1
+    $('.home-slider').slick({
+        lazyLoad: 'ondemand',
+        centerMode: false,
+        focusOnSelect: true,
+        dots: false,
+        loop: true,
+        infinite: true,
+        autoplaySpeed: 3000,
+        autoplay: true,
+        dots: false,
+        arrows: true,
+        slidesToShow: 1
 
-});
+    });
 </script>
 
 <style>
-button.slick-prev.slick-arrow {
-    position: absolute;
-    bottom: 50%;
-    z-index: 99999;
-    left: 0;
-    background: red;
-    color: #fff;
-    border: none;
-}
+    button.slick-prev.slick-arrow {
+        position: absolute;
+        bottom: 50%;
+        z-index: 99999;
+        left: 0;
+        background: red;
+        color: #fff;
+        border: none;
+    }
 
-button.slick-next.slick-arrow {
-    position: absolute;
-    bottom: 50%;
-    z-index: 99999;
-    right: 0;
-    background: red;
-    color: #fff;
-    border: none;
-}
+    button.slick-next.slick-arrow {
+        position: absolute;
+        bottom: 50%;
+        z-index: 99999;
+        right: 0;
+        background: red;
+        color: #fff;
+        border: none;
+    }
 </style>
 
 
 
 <?php
-    include "include/footer.php"
+include "include/footer.php"
 ?>
